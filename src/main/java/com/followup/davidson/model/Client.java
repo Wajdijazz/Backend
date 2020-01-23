ef4c53ed8eb9676652755f487a3b2daace50bb2f
Wajdijazz <mohamedwajdijaziri@gmail.com>
2020-01-23 14:08:25 +0100

    ajout champ contact pour l entité client

--- src/main/java/com/followup/davidson/model/Client.java
+++ src/main/java/com/followup/davidson/model/Client.java
@@ -22,6 +22,8 @@ public class Client {
 
     private String clientName;
 
+    private String clientContact;
+
 
     @OneToMany(mappedBy="client", cascade = CascadeType.ALL, fetch=FetchType.EAGER)
     private List<Project> projects;
